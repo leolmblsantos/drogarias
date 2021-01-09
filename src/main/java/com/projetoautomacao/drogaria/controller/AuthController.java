959877df2bc5403f21e3d3f953478af72771b7ab
Leonardo Santos <leolmblsantos@gmail.com>
2021-01-09 10:01:27 -0300

    Refresh token

---
+++ src/main/java/com/projetoautomacao/drogaria/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.projetoautomacao.drogaria.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoautomacao.drogaria.dto.EmailDTO;
+import com.projetoautomacao.drogaria.security.JWTUtil;
+import com.projetoautomacao.drogaria.security.UserSS;
+import com.projetoautomacao.drogaria.service.AuthService;
+import com.projetoautomacao.drogaria.service.UserService;
+
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+}
